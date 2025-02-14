006babb507a41e47edbf6548a4f31d0bf673d41f
hsu-git <hsu080206@gmail.com>
2025-02-14 09:37:14 +0900

    frontend v1.

--- review-hsu/header_footer_script.js
+++
@@ -1,9 +0,0 @@
-function loadComponent(id, file) {
-  fetch(file)
-    .then((response) => response.text())
-    .then((data) => (document.getElementById(id).innerHTML = data));
-}
-
-// 헤더와 푸터 로드
-loadComponent('header', '/MyTripper/_common/header.html');
-loadComponent('footer', '/MyTripper/_common/footer.html');
