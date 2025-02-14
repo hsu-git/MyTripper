541b3ca17f6431406f0c3a448f742ea5e2a6c2cb
LeeWonGyu <bitamin707@naver.com>
2025-02-14 14:43:02 +0900

    회원 정보 조회 및 수정 기능 추가

--- mypage-lwg/script.js
+++ mypage-lwg/script.js
@@ -23,9 +23,17 @@ function getCurrentPage(page) {
 const dbUrl = 'https://hvkejvcyejfzecclrlkm.supabase.co';
 const apiKey =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh2a2VqdmN5ZWpmemVjY2xybGttIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkyMzY1MzcsImV4cCI6MjA1NDgxMjUzN30.A7NDpEni7CZaY6tjEc0imu7jdNkD07Cz1Cm35MArmFM';
+const dbTableName = 'users';
+
 // 회원 정보 가져오기
+const userData = {
+  id: '',
+  mbti: '',
+  name: '',
+  password: '',
+  user_id: '',
+};
 async function getInfo() {
-  const dbTableName = 'users';
   const name = document.getElementById('infoName');
   const user_id = document.getElementById('infoId');
   const password = document.getElementById('infoPwd');
@@ -41,12 +49,48 @@ async function getInfo() {
     });
 
     const infoDatas = await response.json();
-    name.value = infoDatas[0].name;
-    user_id.value = infoDatas[0].user_id;
-    // password.value = infoDatas[0].password;
-    mbti.value = infoDatas[0].mbti;
+    const index = 2;
+    userData.name = name.value = infoDatas[index].name;
+    userData.user_id = user_id.value = infoDatas[index].user_id;
+    userData.password = password.value = infoDatas[index].password;
+    userData.mbti = mbti.value = infoDatas[index].mbti;
+    userData.id = infoDatas[index].id;
+    console.log(infoDatas);
+    console.log('Get UserData : ', userData);
   } catch (error) {
-    console.error(error); // 콘솔에 오류 출력
+    console.error(error);
+  }
+}
+
+// 회원 정보 수정
+function modifyInfo() {
+  const form = document.getElementById('infoForm');
+
+  try {
+    form.addEventListener('submit', async (event) => {
+      event.preventDefault();
+      userData.mbti = document.getElementById('infoMbti').value;
+      userData.name = document.getElementById('infoName').value;
+      userData.password = document.getElementById('infoPwd').value;
+      userData.user_id = document.getElementById('infoId').value;
+      console.log('Put UserData : ', userData.password);
+
+      const response = await fetch(`${dbUrl}/rest/v1/${dbTableName}?id=eq.${userData.id}`, {
+        method: 'PUT',
+        headers: {
+          apikey: apiKey,
+          Authorization: `Bearer ${apiKey}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userData),
+      });
+
+      const result = await response.json();
+      console.log(result);
+    });
+  } catch (error) {
+    console.error(error);
+    alert('회원 정보 변경에 실패했습니다. 다시 시도해주세요.');
   }
 }
 
@@ -55,10 +99,11 @@ async function getInfo() {
 window.addEventListener('DOMContentLoaded', () => {
   aside_Active();
 
-  // Page 별 데이터 처리
+  // 페이지별 데이터 처리
   switch (curPage) {
     case 'mypage':
       getInfo();
+      modifyInfo();
       break;
     case 'mydocument':
       break;
