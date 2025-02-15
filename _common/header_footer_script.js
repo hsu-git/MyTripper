044117b66c6dd495e6765922058e24861311c532
uiseon98 <3dnjf14dlfzz@gmail.com>
2025-02-15 22:57:58 +0900

    application

--- _common/header_footer_script.js
+++ _common/header_footer_script.js
@@ -26,9 +26,10 @@ function updateHeaderBasedOnLoginStatus() {
 
 function logout() {
   localStorage.removeItem('token'); // localStorage에서 JWT 토큰 제거
+  localStorage.removeItem('user_id');
   updateHeaderBasedOnLoginStatus(); // 헤더 업데이트
   alert("로그아웃 되었습니다.");
-  window.location.href = "../mainpage-PYB/index.html"; // 메인 페이지로 이동 (원하는 페이지로 변경 가능)
+  window.location.href = "/MyTripper/main01-PYB/index.html"; // 메인 페이지로 이동 (원하는 페이지로 변경 가능)
 
   // (선택 사항) 서버에 로그아웃 요청을 보낼 수도 있습니다.
   // fetch("http://localhost:3000/logout", { method: "POST" });
