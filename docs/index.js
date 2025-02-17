8140490a032ff3fb67147a4c21db2c5a463ec3d1
hsu-git <hsu080206@gmail.com>
2025-02-17 20:20:35 +0900

    2

--- docs/index.js
+++ docs/index.js
@@ -141,5 +141,5 @@ loadComponent('footer', '../_common/footer.html');
 
 // "로그인이 필요합니다" 버튼 클릭 이벤트 핸들러 추가
 loginRequiredButton.addEventListener('click', () => {
-  window.location.href = '../uis/login.html';
+  window.location.href = '/MyTripper/uis/login.html';
 });
